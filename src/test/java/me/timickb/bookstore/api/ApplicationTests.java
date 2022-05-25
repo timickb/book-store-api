c3e62aa697d3c4b562cb8338fc57c65fea5f7d09
Taimur Batrutdinov <trbatrutdinov@edu.hse.ru>
2022-05-25 17:11:45 +0300

    Initial commit

---
+++ src/test/java/me/timickb/bookstore/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.timickb.bookstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
