4414850c4cf513f8c14b7a94a33bfb0d5b283feb
Timur Batrutdinov <trbatrutdinov@edu.hse.ru>
2022-06-15 12:23:49 +0300

    Edited README.md

--- mysql_dumps/accounts.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `accounts` (
-                            `id` int(11) NOT NULL,
-                            `balance` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- mysql_dumps/deals.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `deals` (
-                         `id` int(11) NOT NULL,
-                         `book_id` int(11) NOT NULL,
-                         `account_id` int(11) NOT NULL,
-                         `amount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
