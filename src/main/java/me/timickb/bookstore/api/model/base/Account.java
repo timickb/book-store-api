4c6fd48928e47ca60e72cbe1772deaf8c7ebd432
timickb <timickb@yanex.ru>
2022-06-24 17:01:26 +0500

    Excluded passwordHash from account JSON

--- src/main/java/me/timickb/bookstore/api/model/base/Account.java
+++ src/main/java/me/timickb/bookstore/api/model/base/Account.java
@@ -1,15 +1,19 @@
 package me.timickb.bookstore.api.model.base;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 public class Account extends EntityBase {
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Transient
     private String passwordHash;
     private int balance;
 }
